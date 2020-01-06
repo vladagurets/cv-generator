fea5f55c70d24c49c95046b0821a4b2190ae8b6f
vladagurets <aguretsvlad@gmail.com>
2020-01-06 16:08:31 +0000

    Updates

--- static/js/main.9dc6168b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpcv-generator"]=this["webpackJsonpcv-generator"]||[]).push([[0],{3:function(e,n,t){e.exports=t(8)},8:function(e,n,t){"use strict";t.r(n);var r=t(0),a=t.n(r),c=t(2);var o=function(){return a.a.createElement("div",{className:"App"},"Hello")};t.n(c).a.render(a.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.9dc6168b.chunk.js.map
