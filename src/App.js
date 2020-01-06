452b17c414934446992bfbfd6d27609af7d37e8a
Vlad <aguretsvlad@gmail.com>
2020-01-06 18:34:35 +0200

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <h1>CV Generator</h1>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>Create CV</Link>
+          </li>
+          <li>
+            <Link to='/users'>Download CV</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/about'>
+          Home
+        </Route>
+        <Route path='/users'>
+          Create CV
+        </Route>
+        <Route path='/'>
+          Download CV
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
