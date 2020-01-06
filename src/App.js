239be3891aee74f1e705662df50a9973a3055f73
Vlad <aguretsvlad@gmail.com>
2020-01-06 18:30:46 +0200

    Added standard conf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
-import React from "react";
+import React from 'react'
 
 const App = () => {
   return (
     <div>
       <h1>CV Generator</h1>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
